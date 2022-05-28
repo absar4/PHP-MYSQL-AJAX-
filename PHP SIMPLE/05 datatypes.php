92e28f9b1b103fa46f3a37fcf9f205316230f5a3
Absar Ahmad <absarahmad.coder@gmail.com>
2022-05-28 18:52:06 +0500

    data type

---
+++ PHP SIMPLE/05 datatypes.php
@@ -0,0 +1,36 @@
+<!-- 
+    
+1. String
+$x = "Hello world!";
+$y = 'Hello world!';
+
+
+2. Integer
+An integer data type is a non-decimal number between -2,147,483,648 and 2,147,483,647.
+$x = 5985;
+var_dump($x);
+
+3. Float (floating point numbers - also called double)
+$x = 10.365;
+var_dump($x);
+
+4. Boolean
+$x = true;
+$y = false;
+
+5. Array
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+
+6. Object
+We'll look about Objects in coming up videos
+
+
+7. NULL 
+Null is a special data type which can have only one value: NULL.
+$x = "Hello world!";
+$x = null;
+var_dump($x);
+
+-->
+
