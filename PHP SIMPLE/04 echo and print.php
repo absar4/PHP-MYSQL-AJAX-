2a5de907f082bf8a8eb3e88f78c77368257dc28b
Absar Ahmad <absarahmad.coder@gmail.com>
2022-05-28 14:38:25 +0500

    echo and print

---
+++ PHP SIMPLE/04 echo and print.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello THEQUICKCODE";
+echo "Hello", " THEQUICKCODE"; //Multiple values 
+echo "Hello". " THEQUICKCODE"; // single value with concatination
+echo 2233; // integer value
+echo 12.232; // floating
+echo "<h1>This is Runnning HTML ALSO</h1>";
+
+print "Hello THEQUICKCODE";
+print "Hello", " THEQUICKCODE"; //GENERATE ERROR because print can't take multiple values
+print "Hello". " THEQUICKCODE"; // single value with concatination
+print 2233; // integer value
+print 12.232; // floating
+print "<h1>This is Runnning HTML ALSO</h1>";
+
+
+// ECHO is better than print and echo is faster
+?>
